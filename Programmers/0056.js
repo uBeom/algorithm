63df7db3f74ea6f2c4fccdd0564b370b7fd53178
Ubeom Kim <kimub1204@gmail.com>
2022-07-20 15:32:14 +0900

    solved

--- Programmers/0056.js
+++ Programmers/0056.js
@@ -1,34 +1,45 @@
 // 베스트앨범
 function solution(genres, plays) {
   const answer = [];
-  const map = new Map();
-  const set = new Set();
+  const genresTotalPlays = new Map();
 
-  const list = plays.map((e, i) => [genres[i], e, i]);
-  list.sort((a, b) => b[1] - a[1]);
-
-  genres.forEach(e => set.add(e));
-
-  const setArr = Array.from(set);
-  const sequence = Array.from({length: setArr.length}, (_, i) => [setArr[i], 0]);
-
-  genres.forEach((e, i) => sequence[setArr.indexOf(e)][1] += plays[i]);
-  sequence.sort((a, b) => b[1]-a[1]);
-
-  list.forEach(e => {
-    if(!map.has(e[0])) map.set(e[0], [[e[1], e[2]]]);
-    else map.get(e[0]).push([e[1], e[2]]);
+  genres.forEach((genre, index) => {
+    if (genresTotalPlays.has(genre))
+      genresTotalPlays.set(genre, genresTotalPlays.get(genre) + plays[index]);
+    else genresTotalPlays.set(genre, plays[index]);
   });
-  
-  sequence.forEach(e => {
-    if(map.get(e[0]).length === 1) answer.push(map.get(e[0])[0][1]);
-    else {
-      for(let i=0; i<2; i++)
-        answer.push(map.get(e[0])[i][1]);
-    }
-  });
+
+  while (genresTotalPlays.size) {
+    const max = {
+      key: "",
+      value: Number.MIN_SAFE_INTEGER,
+    };
+
+    genresTotalPlays.forEach((play, genre) => {
+      if (play > max.value) {
+        max.key = genre;
+        max.value = play;
+      }
+    });
+
+    genres
+      .map((genre, index) => {
+        if (genre === max.key) return { index, value: plays[index] };
+      })
+      .filter((genreInfo) => genreInfo)
+      .sort((a, b) => b.value - a.value)
+      .slice(0, 2)
+      .forEach((genreInfo) => answer.push(genreInfo.index));
+
+    genresTotalPlays.delete(max.key);
+  }
 
   return answer;
 }
 
- console.log(solution(["classic", "pop", "classic", "classic", "pop"], [500, 600, 150, 800, 2500]));
+console.log(
+  solution(
+    ["classic", "pop", "classic", "classic", "pop"],
+    [500, 600, 150, 800, 2500]
+  )
+);
